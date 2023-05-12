556e097d62677e411d447ef0eddb0d2a24936a0a
kenzo-bt <kenzobalerdigit@gmail.com>
2023-05-12 16:47:10 +0300

    Using a functional component

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <div>
-    <img src="./react-logo.png" height="32px" width="32px" />
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
+function MyComponent() {
+  return (
+    <>
+      <img src="react-logo.png" height="64px" />
+      <h1>Welcome to React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </>
+  );
+}
+
+React.createRoot(document.getElementById("root")).render(
+  <MyComponent />
 );
