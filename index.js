b5237c02badb807e40bbdfc40f3355b5c28f6cf6
kenzo-bt <kenzobalerdigit@gmail.com>
2023-05-12 16:29:00 +0300

    Changed index.html

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <h1>Hello there ;)</h1>
+  <>
+    <img src="./react-logo.png" height="32px" width="32px" />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100K stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </>
 );
